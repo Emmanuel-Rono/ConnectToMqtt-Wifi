b374c7fddfadf80cf41d99b2597272326ea61eb3
Emmanuel-Rono <ronoemmanuel933@gmail.com>
2023-02-17 12:38:43 +0300

    Connection success

--- ConnectWifi&Mqtt/ConnectWifi&Mqtt.ino
+++ ConnectWifi&Mqtt/ConnectWifi&Mqtt.ino
@@ -1,50 +1,41 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 
+WiFiClient wifiClient;
+PubSubClient mqttClient(wifiClient);
+
 // Wifi connection
-const char* ssid="";
-const char* password="";
- //Mqtt connection
- const char* mqttServer="";
- const int mqttPort=3000;
- const char mqttUser="";
- const char * mqttPassword="";
-void setup()
- {
- Serial.begin(9600)
- //connect  wifi 
- Serial.println("Connecting to: ",ssid);
- WiFi.begin(ssid, password);
- while(Wifi.status != WL_CONNECTED() )
- {
- delay(5000)
- {
- serial.println(".")
- }
- }
-
- Serial.println("Connected");
-
- 
- }
-
-void loop()
+const char* ssid = "Accesspoint";
+const char* password = ".CT1.CT2";
+//Mqtt connection
+const char* mqttServer = "broker.hivemq.com";
+ int mqttPort = 8000;
+//const char* mqttUser = "";
+//const char* mqttPassword = "";
+void setup() 
 {
-while(!client.connected())
-{
-	Serial.println("Connecting to MQTT";
+  Serial.begin(9600);
+  //connect  wifi
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to:"); Serial.print(ssid);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(5000);
+    Serial.print(".");
+  }
+  Serial.println("Connected");
 }
-if(client.connect("ESpCLient",mqttUser,mqttPassword)) //can also pass the mqttPort an 
-//and boker depending on the requirement ad ocumented by the specific broker
-
-{
-Serial.println("Connected to mqtt Broker..");
-}
-else
-{
-Serial.println("Failed to connect ");
-Serial.println(client.state())
-delay(5000);
-}
-	
+void loop() {
+  mqttClient.setServer(mqttServer, mqttPort);
+  while (!mqttClient.connected()) {
+    Serial.println("Connecting to MQTT");
+  }
+  if (mqttClient.connect("ESpCLient"))
+  //and boker depending on the requirement ad ocumented by the specific broker
+  {
+    Serial.println("Connected to mqtt Broker..");
+  } else {
+    Serial.println("Failed to connect ");
+    Serial.println(mqttClient.state());
+    delay(5000);
+  }
 }
--- Mqtt.ino
+++ Mqtt.ino
@@ -1,8 +1,50 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+// Wifi connection
+consr char* ssid="";
+const char8 password="";
+ //Mqtt connection
+ const char* mqttServer="";
+ const int mqttPort=;
+ const char mqttUser="";
+ const char * mqttPassword="";
+void setup()
+ {
+ serial.begin(9600)
+ //connect  wifi 
+ Serial.println("Connecting to: ",ssid);
+ WiFi.begin(ssid, password);
+ while(Wifi.status != WL_CONNECTED())
+ {
+ delay(5000)
+ {
+ serial.println(".")
+ }
+ }
+ else {
+ Serial.println("Connected");
+ }
+ 
+ }
+
 void loop()
 {
-	
-}
-void setup()
+while(!client.connected())
 {
+	Serial.println("Connecting to MQTT";
+}
+if(client.connect("ESpCLient",mqttUser,mqttPassword)) //can also pass the mqttPort an 
+//and boker depending on the requirement ad ocumented by the specific broker
+
+{
+Serial.println("Connected to mqtt Broker..");
+}
+else
+{
+Serial.println("Failed to connect ");
+Serial.println(client.state())
+delay(5000);
+}
 	
 }
